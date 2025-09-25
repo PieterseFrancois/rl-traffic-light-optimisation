1c42050df0bc954d2307454fcc1e6d9b673e45c3
Francois Pieterse <francois.pieterse2901@gmail.com>
2025-09-25 19:47:08 +0200

    Implemented function to evaluate a trained scenario.

---
+++ utils/trained_analysis.py
@@ -0,0 +1,34 @@
+from ray.rllib.algorithms.ppo import PPO
+
+from modules.intersection.memory import LogEntry
+
+def evaluate_trained_scenario(
+    trainer: PPO,
+    env_creator,
+    *,
+    max_steps: int,
+) -> dict[str, list[LogEntry]]:
+    """
+    Use RLlib trainer to act. One independent policy per agent (policy id == agent id).
+    """
+    env = env_creator()
+    obs, _infos = env.reset()
+
+    steps = 0
+    while env.agents and steps < max_steps:
+        actions = {}
+        for tls_id, ob in obs.items():
+            act = trainer.compute_single_action(
+                observation=ob, policy_id=tls_id, explore=True
+            )
+            actions[tls_id] = int(act)
+
+        obs, _rewards, _term, _trunc, _infos = env.step(actions)
+
+        steps += 1
+
+    agent_logs: dict[str, list[LogEntry]] = env.get_agent_logs()
+
+    env.close()
+    return agent_logs
+
